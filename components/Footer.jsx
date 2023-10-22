97d87643aa3d9cf8a0874bfe018a5063e41aa718
ainash08 <146757928+ainash08@users.noreply.github.com>
2023-10-22 20:58:17 +0600

    pages, search

---
+++ components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box } from '@chakra-ui/layout'
+
+function Footer() {
+  return (
+    <Box textAlign='center' p='5' color='gray.600' borderTop='1px' borderColor='gray.100'>
+        © 2023 KZProperty, Inc.
+    </Box>
+  )
+}
+
+export default Footer
--- pages/index.js
+++ pages/index.js
@@ -2,37 +2,26 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 import { baseUrl,fetchApi } from '../utils/fetchData.js';
+import Layout from '../components/Layout.jsx';
+import Property from '../components/Property.jsx';
 
 
 const Banner = ({motto, text1, text2, desc1, desc2, btnText, imageUrl}) => 
 (
-  <Flex flexWrap='wrap' justifyContent='center' alignItems='center' py='10'>
-    <Image 
-      width = {500} 
-      height = {300} 
-      src = {imageUrl} 
-      />
-      
-    <Box>
-      <Text color = 'gray.500' fontSize = 'sm' marginX={40}>{motto}</Text>
-      <Text color = 'gray.500' fontSize = 'sm' marginX={40}>{text1}</Text>
-      <Text color = 'gray.500' fontSize = 'sm' marginX={40}>{desc1}</Text>
-      <Button 
-        color='white'
-        bgColor='#3182CE'
-        borderRadius={4}
-        height={40}
-        width={150}
-        border='1px'
-        cursor='pointer'
-        variant = "outline" marginX={40}>{btnText}</Button>
+  <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
+    <Image src={imageUrl} width={500} height={300} />
+    <Box p='5'>
+      <Text color='gray.500' fontSize='sm' fontWeight='medium'>{motto}</Text>
+      <Text fontSize='3xl' fontWeight='bold'>{text1}</Text>
+      <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700'>{desc1}</Text>
+      <Button fontSize='xl' bg="blue.300" color="white">
+        {btnText}
+      </Button>
     </Box>
   </Flex>
 )
 
 export default function Home({propertiesForSale, propertiesForRent}) {
-
-  console.log(propertiesForSale, propertiesForRent)
 
   return (
     <Box>
@@ -45,6 +34,9 @@ export default function Home({propertiesForSale, propertiesForRent}) {
           btnText='Explore Renting'
           imageUrl = 'https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'/>
 
+        <Flex flexWrap='wrap'>{propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
+        </Flex>
+
         <Banner 
           motto='BUY A HOME'
           text1=' Find, Buy & Own Your'
@@ -53,19 +45,21 @@ export default function Home({propertiesForSale, propertiesForRent}) {
           desc2=' villas and more'
           btnText='Explore Buying'
           imageUrl ='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'/>
+
+        <Flex flexWrap='wrap'>{propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
+        </Flex>
+
     </Box>
   )
 }
 
 export async function getStaticProps() {
   const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`);
-
   const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`);
 
   return {
     props: {
       propertiesForSale: propertyForSale?.hits,
-
       propertiesForRent: propertyForRent?.hits,
     },
   };
